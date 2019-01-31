68a6483aad3bd43bf3952be67041bbf70ce2eed6
eunji <twih1203@gmail.com>
2019-01-31 12:12:37 +0900

    first commit

---
+++ EJWhatShouldIWear/EJWhatShouldIWear/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EJWhatShouldIWear
+//
+//  Created by DEV_MOBILE_IOS_JUNIOR on 31/01/2019.
+//  Copyright © 2019 DEV_MOBILE_IOS_JUNIOR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
