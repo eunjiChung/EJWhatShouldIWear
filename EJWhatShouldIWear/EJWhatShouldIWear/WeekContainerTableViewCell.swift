e1726b44523ed1292b1b10619d09f3ae8f5e3544
eunjiChung <twih@naver.com>
2019-07-07 20:12:09 +0900

    Modified to new main UI

---
+++ EJWhatShouldIWear/EJWhatShouldIWear/WeekContainerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  WeekContainerTableViewCell.swift
+//  EJWhatShouldIWear
+//
+//  Created by CHUNGEUNJI on 07/07/2019.
+//  Copyright © 2019 DEV_MOBILE_IOS_JUNIOR. All rights reserved.
+//
+
+import UIKit
+
+class WeekContainerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
