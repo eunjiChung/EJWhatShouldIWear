837ace54296a24c0f45bbc7b0f2c791b1e34e092
eunjiChung <twih1203@gmail.com>
2019-08-23 17:08:26 +0900

    Create New DevInfo View Scene
    # Add new Dev Info TableView
    # Add three tableview cells

--- EJWhatShouldIWear/EJWhatShouldIWear/DevInfoViewController.swift
+++ EJWhatShouldIWear/EJWhatShouldIWear/DevInfoViewController.swift
@@ -10,20 +10,10 @@ import UIKit
 
 class DevInfoViewController: UIViewController {
 
-    
-    @IBOutlet weak var alcTopOfStackView: NSLayoutConstraint!
-    @IBOutlet weak var alcTopOfTextLabel: NSLayoutConstraint!
-    @IBOutlet weak var alcTopOfCopyRight: NSLayoutConstraint!
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        alcTopOfStackView.constant = EJSizeHeight(277.0)
-        alcTopOfTextLabel.constant = EJSizeHeight(130.0)
-        alcTopOfCopyRight.constant = EJSizeHeight(56.0)
+        
+        
     }
     
     @IBAction func didTouchBackBtn(_ sender: Any) {
---
+++ EJWhatShouldIWear/EJWhatShouldIWear/DeveloperTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DeveloperTableViewCell.swift
+//  EJWhatShouldIWear
+//
+//  Created by eunji on 23/08/2019.
+//  Copyright © 2019 DEV_MOBILE_IOS_JUNIOR. All rights reserved.
+//
+
+import UIKit
+
+class DeveloperTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
