ba40d608cc1ccd17c5842f44f0974dd18957c91e
eunjiChung <twih@naver.com>
2019-09-02 22:35:15 +0900

    Wow Nested collectionView.....How can I make it?

--- EJWhatShouldIWear/EJWhatShouldIWear/MainLocationCollectionViewCell.swift
+++ EJWhatShouldIWear/EJWhatShouldIWear/MainLocationCollectionViewCell.swift
@@ -10,4 +10,27 @@ import UIKit
 
 class MainLocationCollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func awakeFromNib() {
+        
+        print("MainLocation")
+        
+        collectionView.dataSource = self as? UICollectionViewDataSource
+        
+        collectionView.register(UINib.init(nibName: "WeatherCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "WeatherCollectionViewCell")
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "WeatherCollectionViewCell", for: indexPath) as! WeatherCollectionViewCell
+        
+        return cell
+    }
+    
+    
 }
---
+++ EJWhatShouldIWear/EJWhatShouldIWear/WeatherCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherCollectionViewCell.swift
+//  EJWhatShouldIWear
+//
+//  Created by CHUNGEUNJI on 02/09/2019.
+//  Copyright © 2019 DEV_MOBILE_IOS_JUNIOR. All rights reserved.
+//
+
+import UIKit
+
+class WeatherCollectionViewCell: UICollectionViewCell {
+    
+    override func awakeFromNib() {
+        print("WeatherCollectionView")
+    }
+}
