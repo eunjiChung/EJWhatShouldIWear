6c381247d3c561a3a1e6cd6e7ec979f5a6f527b9
eunjiChung <twih1203@gmail.com>
2019-09-02 18:53:46 +0900

    Modifying UIs

--- EJWhatShouldIWear/EJWhatShouldIWear/ShowClothTableViewCell.swift
+++ EJWhatShouldIWear/EJWhatShouldIWear/ShowClothTableViewCell.swift
@@ -119,7 +119,7 @@ class ShowClothTableViewCell: UITableViewCell {
         alcHeightOfContainerView.constant = EJSizeHeight(256.0)
         alcLeadingOfStackView.constant = EJSize(64.0)
         alcTrailingOfStackView.constant = EJSize(64.0)
-        alcBottomOfStackView.constant = EJSizeHeight(56.0)
+//        alcBottomOfStackView.constant = EJSizeHeight(56.0)
 //        alcHeightOfHangerImage.constant = EJSizeHeight(146.0)
         alcTopOfFirstClothImageView.constant = EJSizeHeight(15.0)
         alcLeadingOfFirstImageview.constant = EJSize(25.0)
@@ -130,7 +130,7 @@ class ShowClothTableViewCell: UITableViewCell {
         alcBottomOfFirstLabel.constant = EJSizeHeight(15.0)
         alcLeadingOfClothView.constant = EJSize(20.0)
 //        alcTopOfClothView.constant = EJSizeHeight(62.0)
-        alcBottomOfClothView.constant = EJSizeHeight(34.0)
+//        alcBottomOfClothView.constant = EJSizeHeight(34.0)
         alcTrailingOfClothView.constant = EJSize(20.0)
     }
     
