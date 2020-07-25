32137f08f70fdc1c818057a9f73e220b0f9f339d
eunjiChung <>
2020-07-25 23:30:41 +0900

    Add background image helper

---
+++ EJWhatShouldIWear/EJWhatShouldIWear/Manager/EJImageHelper.swift
@@ -0,0 +1,21 @@
+//
+//  EJImageHelper.swift
+//  EJWhatShouldIWear
+//
+//  Created by chungeunji on 2020/07/08.
+//  Copyright © 2020 DEV_MOBILE_IOS_JUNIOR. All rights reserved.
+//
+
+import Foundation
+
+struct EJBackgroundImage {
+    static let background = "background"
+    static let cloud = "cloud"
+    static let dust = "dust"
+    static let clear = "clear"
+    static let cloudy = "cloudy"
+    static let night = "night"
+    static let rainy = "rainy"
+    static let snow = "snow"
+    static let sunset = "sunset"
+}
